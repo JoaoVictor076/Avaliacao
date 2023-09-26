d2f14398274b35cc02a41eab58de44127a90c412
JoaoVictor076 <joaokz10@outlook.com>
2023-09-26 20:06:52 -0300

    validações

--- Avaliacao/Models/RegistroDeCartasViewModel.cs
+++ Avaliacao/Models/RegistroDeCartasViewModel.cs
@@ -8,7 +8,8 @@ namespace Avaliacao.Models
         [MinLength(3, ErrorMessage = "Minimo de 3 letras.")]
         [MaxLength(255, ErrorMessage = "maximo ....")]
         public string Nome { get; set; }
-        
+
+        [Required(ErrorMessage = "O endereço é obrigatorio...") ]
         public string  Endereco { get; set; }
 
         [Range(1,15)]
